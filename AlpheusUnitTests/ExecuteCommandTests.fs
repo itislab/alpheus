a22940d6cc115e325c9c8e12242054da3ff8d0e5
Dmitry Grechka <dmitry@grechka.family>
2019-07-17 20:05:52 +0300

    workaround for issue #1. fixes #1. Tests for local execution

--- AlpheusUnitTests/ExecuteCommandTests.fs
+++ AlpheusUnitTests/ExecuteCommandTests.fs
@@ -8,3 +8,35 @@ open ItisLab.Alpheus.Tests.Utils
 open ItisLab.Alpheus
 open ItisLab.Alpheus.ExecuteCommand
 
+[<Fact>]
+let ``Running utility from PATH``() =
+    async {
+        let! exitCode = ExecuteCommand.runCmdLocallyAsync (fun x -> printfn "%s" x) "test" "dotnet" "--list-runtimes" Environment.CurrentDirectory
+        Assert.Equal(0,exitCode)
+        } |> toAsyncFact
+
+type FileCopingTests()=
+    inherit SingleUseOneTimeDirectory()
+
+    [<Fact>]
+    member s.``Running local executable``() =
+        async {
+            let localExecutable =
+                if isTestRuntimeWindows then "copy.cmd" else "copy.sh"
+            let wd = Path.GetFullPath(Path.Combine(s.Path,"../../")) // this is "data" dir.
+            // Test checks that the executable is searched here,
+            // and that executable's working directory is set properly
+            // Note that this wd is different from test runtime working directory
+            let newLocation = Path.GetFullPath(Path.Combine(s.Path,"texturalData.txt"))
+            let args = sprintf "texturalData.txt %s" newLocation
+
+            let tracefn s =
+                System.Diagnostics.Trace.WriteLine(s)
+
+            sprintf "command: %s" localExecutable |> tracefn
+            sprintf "args: %s" args |> tracefn
+            sprintf "wd: %s" wd |> tracefn
+
+            let! exitCode = ExecuteCommand.runCmdLocallyAsync (fun x -> System.Diagnostics.Trace.WriteLine(sprintf "%s" x)) "test" localExecutable args wd
+            Assert.Equal(0,exitCode)
+        }
