24b5d5727d6a3bd98c9505236130fa60196788db
Dmitry Grechka <dmitry@grechka.family>
2019-07-18 18:31:33 +0300

    API test: building dependency graph

---
+++ AlpheusUnitTests/ApiTests.fs
@@ -0,0 +1,35 @@
+﻿module ItisLab.Alpheus.Tests.ApiTests
+
+open Xunit
+open ItisLab.Alpheus.Tests
+open ItisLab.Alpheus.Tests.Utils
+open ItisLab.Alpheus.API
+open System.Collections
+open System.Collections.Generic
+open ItisLab.Alpheus.AlphFiles
+
+/// can be used as calssData for XUnit theory. returns all of the artefactIDs for the sample experiment
+type ArtefactIdSource() =
+    let sampleExperiment = new SampleExperiment.SampleExperiment()
+    let sequence : IEnumerable<obj array> = sampleExperiment.FullArtIds |> Seq.map (fun x -> x :> obj) |> Seq.map (fun x -> [|x|])
+    interface IEnumerable<obj array> with
+        member s.GetEnumerator() =
+            sequence.GetEnumerator()
+
+    interface IEnumerable with
+        member s.GetEnumerator() =
+            sequence.GetEnumerator() :> IEnumerator
+            
+
+type DepGraphConstruction() =
+    inherit SampleExperiment.SampleExperiment()
+
+    [<Theory>]
+    [<ClassData(typedefof<ArtefactIdSource>)>]
+    member s.``dependencyGraph loads``(artefactId:ArtefactId) =
+        async {
+            traceInfo (sprintf "testing %A" artefactId)
+            let! graph = buildDependencyGraphAsync s.RootPath artefactId
+            Assert.True(graph.ArtefactsCount>0,"Graph must be non-empty")
+            } |> toAsyncFact
+
--- AlpheusUnitTests/DependencyGraphDiskTests.fs
+++ AlpheusUnitTests/DependencyGraphDiskTests.fs
@@ -6,85 +6,15 @@ open Xunit
 open ItisLab.Alpheus
 open Utils
 
-type SampleExperiment() =
-    inherit SingleUseOneTimeDirectory()
-
-    // creating sample experiment folder for tests
-    let artIdsStr = 
-        [|
-            @"dir1/test1.txt"
-            @"dir1/test2/"
-            @"dir2/test3.txt"
-            @"dir2/test4/"
-            @"dir3/dir5/test5.txt"
-        |]
-
-    let fullArtIds = Array.map ItisLab.Alpheus.AlphFiles.ArtefactId.ID artIdsStr
-
-    let rootPath = Path.GetFullPath(``base``.Path)
-
-    do
-
-        ItisLab.Alpheus.Config.createExperimentDirectoryAsync rootPath |> Async.RunSynchronously |> ignore
-
-        let fullPaths = Array.map (fun x -> fullIDtoFullPath rootPath x) fullArtIds
-        let fullAlphFilePaths = Array.map (fun x -> artefactPathToAlphFilePath x) fullPaths
-
-        // creating dirs
-        Directory.CreateDirectory(Path.Combine(``base``.Path,"dir1")) |> ignore
-        Directory.CreateDirectory(Path.Combine(``base``.Path,"dir1","test2")) |> ignore
-        Directory.CreateDirectory(Path.Combine(``base``.Path,"dir2")) |> ignore
-        Directory.CreateDirectory(Path.Combine(``base``.Path,"dir3")) |> ignore
-        Directory.CreateDirectory(Path.Combine(``base``.Path,"dir2","test4")) |> ignore
-        Directory.CreateDirectory(Path.Combine(``base``.Path,"dir3","dir5")) |> ignore
-
-        // creating files with content
-        fullPaths
-            |> Array.filter (fun (x:string) -> x.EndsWith("txt"))
-            |> Array.iteri (fun i x -> File.WriteAllText(x,sprintf "data file %d" i) )
-
-        // creating graph
-        let g = DependencyGraph.Graph()
-
-        // allocating artefact verticeis
-        let artefacts = Array.map g.GetOrAllocateArtefact fullArtIds
-
-        // filling in actual hashes
-        ItisLab.Alpheus.DependencyGraph.fillinActualHashesAsync artefacts rootPath |> Async.RunSynchronously
-
-        let versionedArtefacts = Array.map ItisLab.Alpheus.DependencyGraph.getVersionedArtefact artefacts
-
-        // connecting artefacts with methods
-        // actual dependency links are created here
-        let source1 = DependencyGraph.Source(g.AllocateSourceMethod fullArtIds.[0])
-        artefacts.[0].ProducedBy <- source1
-        let source2 = DependencyGraph.Source(g.AllocateSourceMethod fullArtIds.[1])
-        artefacts.[1].ProducedBy <- source2
-        let method3 = g.AddMethod [versionedArtefacts.[0]; versionedArtefacts.[1]] [versionedArtefacts.[2]]
-        let method4 = g.AddMethod [versionedArtefacts.[0]; versionedArtefacts.[1]] [versionedArtefacts.[3]]
-        let method5 = g.AddMethod [versionedArtefacts.[2]; versionedArtefacts.[3]] [versionedArtefacts.[4]]
-
-        // alph file content
-        let alphFiles =
-            let artToAlphFile artefact alphFilePath =
-                ItisLab.Alpheus.DependencyGraph.artefactToAlphFile artefact alphFilePath rootPath
-            Array.map2 artToAlphFile artefacts fullAlphFilePaths
-
-        // dumping alph files to disk
-        Array.iter2 (fun alph path -> saveAsync alph path |> Async.RunSynchronously) alphFiles fullAlphFilePaths
-
-        // deleting first artefact alph file emulating the file without alph files
-        File.Delete(fullAlphFilePaths.[0])
-
-        // delete forth artefact to emulate dir absence
-        Directory.Delete(fullPaths.[3])
+type Tests() =
+    inherit SampleExperiment.SampleExperiment()
 
     [<Fact>]
     member s.``LoadDependenciesAsync loads single source file artefact``() =
         async {
             let g = DependencyGraph.Graph()
-            let artefact = g.GetOrAllocateArtefact fullArtIds.[0]
-            let! artefactVertices = g.LoadDependenciesAsync [artefact] rootPath
+            let artefact = g.GetOrAllocateArtefact s.FullArtIds.[0]
+            let! artefactVertices = g.LoadDependenciesAsync [artefact] s.RootPath
 
             Assert.Equal(1,artefactVertices.Count)
             Assert.Equal(artefact,Seq.exactlyOne artefactVertices)
@@ -94,8 +24,8 @@ type SampleExperiment() =
     member s.``LoadDependenciesAsync loads single source dir artefact``() =
         async {
             let g = DependencyGraph.Graph()
-            let artefact = g.GetOrAllocateArtefact fullArtIds.[1]
-            let! artefactVertices = g.LoadDependenciesAsync [artefact] rootPath
+            let artefact = g.GetOrAllocateArtefact s.FullArtIds.[1]
+            let! artefactVertices = g.LoadDependenciesAsync [artefact] s.RootPath
 
             Assert.Equal(1,artefactVertices.Count)
             Assert.Equal(artefact,Seq.exactlyOne artefactVertices)
@@ -105,10 +35,10 @@ type SampleExperiment() =
     member s.``LoadDependenciesAsync loads two source artefacts``() =
         async {
             let g = DependencyGraph.Graph()
-            let artefactsIdsToTest = Array.take 2 fullArtIds
+            let artefactsIdsToTest = Array.take 2 s.FullArtIds
             let artefactToTest = Array.map g.GetOrAllocateArtefact artefactsIdsToTest
 
-            let! resultingVertices = g.LoadDependenciesAsync (List.ofArray artefactToTest) rootPath
+            let! resultingVertices = g.LoadDependenciesAsync (List.ofArray artefactToTest) s.RootPath
 
             Assert.Equal(2,resultingVertices.Count)
             Assert.Equal<DependencyGraph.ArtefactVertex>(Set.ofArray artefactToTest, resultingVertices)
@@ -118,8 +48,8 @@ type SampleExperiment() =
     member s.``LoadDependenciesAsync loads single computed file artefact``() =
         async {
             let g = DependencyGraph.Graph()
-            let artefact = g.GetOrAllocateArtefact fullArtIds.[2]
-            let! resultingVertices = g.LoadDependenciesAsync [artefact] rootPath
+            let artefact = g.GetOrAllocateArtefact s.FullArtIds.[2]
+            let! resultingVertices = g.LoadDependenciesAsync [artefact] s.RootPath
 
             Assert.Equal(3,resultingVertices.Count)
         } |> toAsyncFact
@@ -128,8 +58,8 @@ type SampleExperiment() =
     member s.``LoadDependenciesAsync loads single computed dir artefact``() =
         async {
             let g = DependencyGraph.Graph()
-            let artefact = g.GetOrAllocateArtefact fullArtIds.[3]
-            let! resultingVertices = g.LoadDependenciesAsync [artefact] rootPath
+            let artefact = g.GetOrAllocateArtefact s.FullArtIds.[3]
+            let! resultingVertices = g.LoadDependenciesAsync [artefact] s.RootPath
 
             Assert.Equal(3,resultingVertices.Count)
         } |> toAsyncFact
@@ -138,12 +68,12 @@ type SampleExperiment() =
     member s.``LoadDependenciesAsync loads full graph``() =
         async {
             let g = DependencyGraph.Graph()
-            let artefact = g.GetOrAllocateArtefact fullArtIds.[4]
-            let! resultingVertices = g.LoadDependenciesAsync [artefact] rootPath
+            let artefact = g.GetOrAllocateArtefact s.FullArtIds.[4]
+            let! resultingVertices = g.LoadDependenciesAsync [artefact] s.RootPath
 
             let resultingIDs = Seq.map (fun (x:DependencyGraph.ArtefactVertex) -> x.Id) resultingVertices |> Set.ofSeq
 
             Assert.Equal(5,resultingVertices.Count)
-            Assert.Equal<ArtefactId>(resultingIDs,Set.ofSeq fullArtIds)
+            Assert.Equal<ArtefactId>(resultingIDs,Set.ofSeq s.FullArtIds)
         } |> toAsyncFact
 
