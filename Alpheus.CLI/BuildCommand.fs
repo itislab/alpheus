c0a1593defdd930e1418cb9e27f5ec7ea462e206
Dmitry Grechka <dmitry@grechka.family>
2020-01-20 21:23:42 +0300

    ability to set resource group with build CLI command

--- Alpheus.CLI/BuildCommand.fs
+++ Alpheus.CLI/BuildCommand.fs
@@ -23,12 +23,13 @@ let run workingDir (buildArgs:ParseResults<BuildArgs>) =
             { 
                 DependencyGraph.CommandExecutionSettings.Default with
                     DoNotCleanOutputs = buildArgs.Contains Disable_Outputs_Clean
+
             }
 
         if settings.DoNotCleanOutputs then
             Logger.logVerbose Logger.CLI "Clearing of outputs is disabled for this command execution"
 
-        let settings2 =
+        let settings =
             match buildArgs.TryGetResult Successful_Exit_Codes with
             |   Some(codes) ->
                 Logger.logVerbose Logger.CLI (sprintf "Custom exit codes are considered as successful exit: %A" codes)
@@ -36,9 +37,17 @@ let run workingDir (buildArgs:ParseResults<BuildArgs>) =
             |   None ->
                 settings
 
+        let settings =
+            match buildArgs.TryGetResult Resource_Group with
+            |   Some(groups) ->
+                Logger.logVerbose Logger.CLI (sprintf "Resource groups were set: %A" groups)
+                {settings with ResourceGroups = set groups }
+            |   None ->
+                settings
+
         match roots with
         | [] -> return! Error (UserError "An output artefact is not specified")
         | [None] -> return! Error (UserError "Not an experiment folder: .alpheus")
-        | [Some experimentRoot] -> return! (API.buildAsync experimentRoot workingDir deps outputs command settings2 |> Async.RunSynchronously)
+        | [Some experimentRoot] -> return! (API.buildAsync experimentRoot workingDir deps outputs command settings |> Async.RunSynchronously)
         | _ -> return! Error (UserError "Not all of the input or output artefacts are under the same experiment root folder")
     }
